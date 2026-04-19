0a28b032c718cb131c92afa53121f44964e50d4e
a44icus <a44icus@gmail.com>
2026-04-19 19:00:09 +0200

    fix: remove basePath for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/terranova', // ← adapte selon ton sous-dossier
   images: {
     remotePatterns: [
       {
