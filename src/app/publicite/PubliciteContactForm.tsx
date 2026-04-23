c942973696bf2277fb886f2abd5f4837c3777dc8
a44icus <a44icus@gmail.com>
2026-04-24 00:36:58 +0200

    feat: update complet du projet

---
+++ src/app/publicite/PubliciteContactForm.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function PubliciteContactForm() {
+  return (
+    <div>
+      <h2>Formulaire de contact publicité</h2>
+      <p>Formulaire en cours de création...</p>
+    </div>
+  )
+}
